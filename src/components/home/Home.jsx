e5111b449e9f49e28f2465dcf794199f14435cf2
sherlingdev <sherlingdev@gmail.com>
2024-11-28 06:52:23 -0500

    first commit

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+import profileImg from '../../assets/profile-img.png'
+import shapeOne from '../../assets/shape-1.png'
+import shapeTwo from '../../assets/shape-2.png'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
